088f0d7f67ddd96419e5bfd7066504ec9d5af65b
shun <zhongshun89@outlook.com>
2020-11-29 14:24:34 +0800

    test

--- test.sh
+++ test.sh
@@ -3,4 +3,4 @@
 DOMAINS="test.threegems.net jenkins.threegems.net"
 EMAIL="zhongshun89@outlook.com"
 
-./register_ssl.sh --domains $DOMAINS --email $EMAIL --data-path ./certbot --staging 1
+./register_ssl.sh --domains "test.threegems.net jenkins.threegems.net" --email "zhongshun89@outlook.com" --data-path ./certbot --staging 1
